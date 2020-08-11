3b75e4c902f22b371e3a919e4bd287a5eb076fe3
Jia Ren <st870794@gmail.com>
2020-08-11 14:53:24 +0800

    add filterimg

--- read.py
+++ read.py
@@ -10,7 +10,15 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 sum_len = 0
-for d in data: # data裡有d個字串(d篇留言)
+for d in data: # 清單data裡有d個字串(d篇留言)
 	sum_len = sum_len + len(d) # 每篇字數加總
 print('平均每筆留言長度為', sum_len/len(data)) 
 #後面除以data篇留言(100萬筆)
+
+# 篩選留言長度
+new =[]
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100字')
+print(new[0])
