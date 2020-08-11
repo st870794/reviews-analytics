8a6d2a93c8cd55be6be0a553408ea0959d93ee17
Jia Ren <st870794@gmail.com>
2020-08-11 14:45:30 +0800

    add calculate avg reviews

--- read.py
+++ read.py
@@ -4,11 +4,13 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # 快速計數
-		if count % 1000 == 0: #除1000餘0時數一次
+		if count % 10000 == 0: #除1000餘0時數一次
 		# %是指餘數，譬如10 % 6 = 4(10除6餘4)；15 % 15 = 0
-		print(len(data))
-print(len(data))
+			print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
-print('-----------------')
-print(data[1])
+sum_len = 0
+for d in data: # data裡有d個字串(d篇留言)
+	sum_len = sum_len + len(d) # 每篇字數加總
+print('平均每筆留言長度為', sum_len/len(data)) 
+#後面除以data篇留言(100萬筆)
